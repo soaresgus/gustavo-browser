a91b8fdee7063dfc800d498298620d335c661f94
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-07 19:47:25 -0300

    Create destine url logic;

--- src/script.js
+++ src/script.js
@@ -1,1 +1,30 @@
-/* feito a base do navegador */
+document.addEventListener('DOMContentLoaded', () => {
+    const urlBar = document.querySelector('#url-bar');
+    const urlButton = document.querySelector('#url-button');
+
+    function switchPage(value) {
+        let httpRemoveRegex = new RegExp(/(^\w+:|^)\/\//)
+        let url = value.replace(httpRemoveRegex, '');
+        let finalUrl = url;
+
+        if (finalUrl.split('.').length <= 1 && !finalUrl.includes(':')) {
+            finalUrl += '.com'
+        }
+
+        if (url != '') {
+            window.location = `http://${finalUrl}`;
+        }
+    }
+
+    urlButton.addEventListener('click', () => {
+        switchPage(urlBar.value);
+    })
+
+    urlBar.addEventListener('keydown', (event) => {
+        let isEnterKey = event.key.toLowerCase() == 'enter'
+
+        if (isEnterKey) {
+            switchPage(urlBar.value);
+        }
+    })
+})  
