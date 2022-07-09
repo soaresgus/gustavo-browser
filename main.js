cc2a9f32712540a1ef1289ac69fd8f114f55837e
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-08 22:50:20 -0300

    Create news shortcuts;

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const { app, BrowserWindow, globalShortcut, Menu, MenuItem } = require('electron')
+const { app, BrowserWindow, screen, Menu, MenuItem } = require('electron')
 
 let win;
-
-require('electron-reloader')(module)
 
 function createWindow() {
     win = new BrowserWindow({
         width: 800,
         height: 600,
         alwaysOnTop: true,
-        autoHideMenuBar: true,
+        autoHideMenuBar: false,
         icon: __dirname + '/assets/logo.png',
         webPreference: {
             nodeIntegration: true
@@ -20,8 +18,40 @@ function createWindow() {
     win.webContents.openDevTools();
 }
 
+function moveWindow(side) {
+    let display = screen.getPrimaryDisplay();
+    let displayWidth = display.bounds.width;
+    let displayHeight = display.bounds.height;
+
+    let windowWidth = win.getSize()[0];
+    let windowHeight = win.getSize()[1];
+
+    let windowXPosition = win.getPosition()[0];
+    let windowYPosition = win.getPosition()[1];
+
+    if (side === 'left') {
+        win.setPosition(0, windowYPosition);
+    }
+
+    if (side === 'right') {
+        win.setPosition(displayWidth - windowWidth, windowYPosition);
+    }
+
+    if (side === 'top') {
+        win.setPosition(windowXPosition, 0);
+    }
+
+    if (side === 'bottom') {
+        win.setPosition(windowXPosition, displayHeight - windowHeight);
+    }
+}
+
 function toggleDevTools() {
     win.webContents.toggleDevTools();
+}
+
+function toggleFixed() {
+    win.setAlwaysOnTop(!win.isAlwaysOnTop());
 }
 
 function returnHome() {
@@ -33,14 +63,46 @@ menu.append(new MenuItem({
     label: 'Gustavo Browser',
     submenu: [
         {
+            label: 'Back to home',
             role: 'Home',
             accelerator: process.platform === 'darwin' ? 'Cmd+H' : 'Ctrl+H',
             click: () => { returnHome() }
         },
         {
+            label: 'Toggle DevTools',
             role: 'Toggle Dev Tools',
             accelerator: process.platform === 'darwin' ? 'Cmd+J' : 'Ctrl+J',
             click: () => { toggleDevTools() }
+        },
+        {
+            label: 'Toggle Picture-In-Picture',
+            role: 'Toggle Picture-in-picture',
+            accelerator: process.platform === 'darwin' ? 'Cmd+F' : 'Ctrl+F',
+            click: () => { toggleFixed() }
+        },
+        {
+            label: 'Move window to left',
+            role: 'Move window to left',
+            accelerator: process.platform === 'darwin' ? 'Cmd+Left' : 'Ctrl+Left',
+            click: () => { moveWindow('left') }
+        },
+        {
+            label: 'Move window to right',
+            role: 'Move window to right',
+            accelerator: process.platform === 'darwin' ? 'Cmd+Right' : 'Ctrl+Right',
+            click: () => { moveWindow('right') }
+        },
+        {
+            label: 'Move window to bottom',
+            role: 'Move window to bottom',
+            accelerator: process.platform === 'darwin' ? 'Cmd+Down' : 'Ctrl+Down',
+            click: () => { moveWindow('bottom') }
+        },
+        {
+            label: 'Move window to top',
+            role: 'Move window to top',
+            accelerator: process.platform === 'darwin' ? 'Cmd+Up' : 'Ctrl+Up',
+            click: () => { moveWindow('top') }
         },
     ]
 }))
